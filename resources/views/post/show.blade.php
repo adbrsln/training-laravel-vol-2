dd2ef7cb0a9a7681e8058ad35a7007786fdaac05
Muhammad Adib Ahmad Roslan <adbroslan@gmail.com>
2020-10-08 02:08:56 +0800

    update create and show ui

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -7,10 +7,16 @@
     <div class="row">
         <div class="col-12">
             <label for="Title">Title</label>
-            <input type="text" name="title" value="{{ $post->title }}" class="form-control mb-4">
+            <input type="text" name="title" value="{{ old('title') ?? $post->title }}" class="form-control @error('title') is-invalid @enderror">
+            @error('title')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
 
-            <label for="Title">Title</label>
-            <textarea name="body" class="form-control" id="body" cols="30" rows="10">{{  $post->body }}</textarea>
+            <label for="Body" class="mt-4">Body</label>
+            <textarea name="body" class="form-control @error('body') is-invalid @enderror" id="body" cols="30" rows="10">{{   old('body') ?? $post->body }}</textarea>
+            @error('body')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
 
             <button type="submit" class=" mt-4 btn btn-primary mt-4 float-right">Submit</button>
         </div>
